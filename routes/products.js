ca8799d7e4bd31e1c42a90dda911c7139ddc6590
Laqwanda Nettles <nettleslaqwanda@gmail.com>
2024-12-16 20:31:10 -0600

    Added stub routes for adding a product, get by id, update by id, and delete by id.

--- routes/products.js
+++ routes/products.js
@@ -8,4 +8,26 @@ productRoutes.get("/", (req, res) => {
   });
 });
 
+// Create a new product
+productRoutes.post("/", (req, res) => {
+  res.json({ message: "Added a new product" });
+});
+
+//Get a single product by ID
+productRoutes.get("/:id", (req, res) => {
+  res.json({ message: "Get product by ID" });
+});
+
+//Update a product by ID
+productRoutes.put("/:id", (req, res) => {
+  res.json({
+    message: "Updated product",
+  });
+});
+
+//Delete a product by ID
+productRoutes.delete("/:id", (req, res) => {
+  res.json({ message: "Deleted product" });
+});
+
 module.exports = productRoutes;
