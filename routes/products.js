deab0e9338cadcdd53b653e6c66d1dfebd924ced
Laqwanda Nettles <nettleslaqwanda@gmail.com>
2024-12-16 22:57:36 -0600

    Removed foundational (test) code and implemented routes.

--- routes/products.js
+++ routes/products.js
@@ -1,33 +1,90 @@
 const Router = require("express").Router;
 const productRoutes = Router();
+const Product = require("../models/product");
 
 // Get all products
-productRoutes.get("/", (req, res) => {
-  res.json({
-    message: "Get all products",
-  });
+productRoutes.get("/", async (req, res) => {
+  try {
+    const products = await Product.find();
+    res.json(products);
+  } catch (error) {
+    console.error("Error retrieving all products: ", error);
+    res.status(500).json({ error: error.message });
+  }
 });
 
 // Create a new product
-productRoutes.post("/", (req, res) => {
-  res.json({ message: "Added a new product" });
+productRoutes.post("/", async (req, res) => {
+  const data = req.body;
+  try {
+    const newProduct = new Product({
+      name: data.name,
+      description: data.description,
+      price: data.price,
+      category: data.category,
+    });
+
+    //save to database
+    const savedProduct = await newProduct.save();
+
+    res.json(savedProduct);
+  } catch (error) {
+    console.error("Error adding new product: ", error);
+    res.status(500).json({ error: error.message });
+  }
 });
 
 //Get a single product by ID
-productRoutes.get("/:id", (req, res) => {
-  res.json({ message: "Get product by ID" });
+productRoutes.get("/:id", async (req, res) => {
+  try {
+    const productId = req.params.id;
+    const product = await Product.findById(productId);
+
+    if (!product) {
+      return res.status(404).json({ error: "Product not found" });
+    }
+
+    res.status(200).json(product);
+  } catch (error) {
+    console.error("Error retrieving product: ", error);
+    res.status(500).json({ error: error.message });
+  }
 });
 
 //Update a product by ID
-productRoutes.put("/:id", (req, res) => {
-  res.json({
-    message: "Updated product",
-  });
+productRoutes.put("/:id", async (req, res) => {
+  try {
+    const product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!product) {
+      return res.status(404).json({ error: "Product not found" });
+    }
+
+    res.json(product);
+  } catch (error) {
+    console.error("Error updating product: ", error);
+    res.status(400).json({ error: error.message });
+  }
 });
 
 //Delete a product by ID
-productRoutes.delete("/:id", (req, res) => {
-  res.json({ message: "Deleted product" });
+productRoutes.delete("/:id", async (req, res) => {
+  try {
+    const productId = req.params.id;
+    const product = await Product.findByIdAndDelete(productId);
+
+    if (!product) {
+      return res.status(404).json({ error: "Product not found" });
+    }
+
+    res.status(200).json({ message: "Product deleted successfully", product });
+  } catch (error) {
+    console.error("Error deleting product: ", error);
+    res.status(500).json({ error: error.message });
+  }
 });
 
 module.exports = productRoutes;
