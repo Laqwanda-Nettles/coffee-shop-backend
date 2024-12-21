13a794504e07690a28e293dc75b0de64940bcc44
Laqwanda Nettles <nettleslaqwanda@gmail.com>
2024-12-21 15:54:04 -0600

    added validation middleware in the post + put routes

--- routes/products.js
+++ routes/products.js
@@ -4,36 +4,44 @@ const Product = require("../models/product");
 const upload = require("../middleware/upload");
 const auth = require("../middleware/auth");
 const role = require("../middleware/role");
+const { validateProduct } = require("../middleware/validate");
 
 // // Create a new product w/ image upload (admin only)
-productRoutes.post("/", auth, role("admin"), upload, async (req, res) => {
-  if (!req.file) {
-    console.error("File not uploaded");
-    return res.status(400).json({ error: "File upload failed" });
+productRoutes.post(
+  "/",
+  auth,
+  role("admin"),
+  upload,
+  validateProduct,
+  async (req, res) => {
+    if (!req.file) {
+      console.error("File not uploaded");
+      return res.status(400).json({ error: "File upload failed" });
+    }
+
+    try {
+      const { name, description, price, category, stock } = req.body;
+      const imageUrl = req.file ? `/uploads/${req.file.filename}` : "";
+
+      const product = new Product({
+        name,
+        description,
+        price,
+        category,
+        stock,
+        imageUrl,
+      });
+
+      //save to database
+      await product.save();
+
+      res.status(201).json(product);
+    } catch (error) {
+      console.error("Error adding new product: ", error);
+      res.status(400).json({ error: error.message });
+    }
   }
-
-  try {
-    const { name, description, price, category, stock } = req.body;
-    const imageUrl = req.file ? `/uploads/${req.file.filename}` : "";
-
-    const product = new Product({
-      name,
-      description,
-      price,
-      category,
-      stock,
-      imageUrl,
-    });
-
-    //save to database
-    await product.save();
-
-    res.status(201).json(product);
-  } catch (error) {
-    console.error("Error adding new product: ", error);
-    res.status(400).json({ error: error.message });
-  }
-});
+);
 
 // Get all products with pagination and filter by category
 productRoutes.get("/", async (req, res) => {
@@ -86,33 +94,40 @@ productRoutes.get("/:id", async (req, res) => {
 });
 
 //Update a product by ID
-productRoutes.put("/:id", auth, role("admin"), upload, async (req, res) => {
-  try {
-    const { name, description, price, category, stock } = req.body;
+productRoutes.put(
+  "/:id",
+  auth,
+  role("admin"),
+  upload,
+  validateProduct,
+  async (req, res) => {
+    try {
+      const { name, description, price, category, stock } = req.body;
 
-    const imageUrl = req.file
-      ? `/uploads/${req.file.filename}`
-      : req.body.imageUrl;
+      const imageUrl = req.file
+        ? `/uploads/${req.file.filename}`
+        : req.body.imageUrl;
 
-    const product = await Product.findByIdAndUpdate(
-      req.params.id,
-      { name, description, price, category, stock, imageUrl },
-      {
-        new: true,
-        runValidators: true,
+      const product = await Product.findByIdAndUpdate(
+        req.params.id,
+        { name, description, price, category, stock, imageUrl },
+        {
+          new: true,
+          runValidators: true,
+        }
+      );
+
+      if (!product) {
+        return res.status(404).json({ error: "Product not found" });
       }
-    );
 
-    if (!product) {
-      return res.status(404).json({ error: "Product not found" });
+      res.json(product);
+    } catch (error) {
+      console.error("Error updating product: ", error);
+      res.status(400).json({ error: error.message });
     }
-
-    res.json(product);
-  } catch (error) {
-    console.error("Error updating product: ", error);
-    res.status(400).json({ error: error.message });
   }
-});
+);
 
 //Delete a product by ID
 productRoutes.delete("/:id", async (req, res) => {
