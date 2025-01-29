93cac5ba9581520987ba809c37281aaf9702add6
Laqwanda Nettles <nettleslaqwanda@gmail.com>
2025-01-28 21:25:18 -0600

    added user to login

--- routes/auth.js
+++ routes/auth.js
@@ -33,7 +33,10 @@ router.post("/login", async (req, res) => {
       process.env.JWT_SECRET,
       { expiresIn: "1h" }
     );
-    res.json({ token });
+    res.json({
+      token,
+      user: { name: user.name, email: user.email, role: user.role },
+    });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
